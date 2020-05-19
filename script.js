0d4d7d7d8dc36264f827db87708524a107773509
Harshit Gupta <hgupta-be-ip19@sgsits.ac.in>
2020-05-20 00:11:20 +0530

    Adds the skeleton files for the page
    Adds the base files over which (definitely includes these files and not just limited to) web development will be done.

---
+++ script.js
@@ -0,0 +1,9 @@
+try{
+    // JS Code
+    
+}
+
+catch(err){
+    // Error-Handling Error-Free JS
+    console.log("Unable to load Esummit.js Completely! Error:", err.message, ". [ In detail:",  err, ". ]\nYou may face some performance limitations of this page.");
+}
